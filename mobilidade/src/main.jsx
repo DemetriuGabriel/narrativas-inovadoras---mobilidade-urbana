fbbb15a6055c8f8b7b57a547ddf519ad99916e97
Weldon <weldonbarros179@gmail.com>
2025-12-05 17:22:38 -0300

    feat: add mobile alarm screen page

--- mobilidade/src/main.jsx
+++ mobilidade/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AlarmScreen from "./AlarmScreen.jsx";
 
-createRoot(document.getElementById('root')).render(
+function Root() {
+  const [showAlarm, setShowAlarm] = useState(true);
+
+  return (
+    <>
+      {showAlarm ? (
+        <AlarmScreen onDismiss={() => setShowAlarm(false)} />
+      ) : (
+        <App />
+      )}
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);
